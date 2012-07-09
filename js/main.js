8e628d32bb8258ad4634e68be01089a201832a2e
pete gamache <pete@where.com>
2012-07-09 19:24:10 -0400

    halfway convincing section logic

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+  var show_section = function show_section (section) {
+    $('#main > div').hide();
+    $(section).show();
+  };
+
+  // install handlers for section links and top logo
+  $('nav a').click(function(){
+    var section = this.getAttribute('href');
+    show_section(section);
+    return false;
+  });
+  $('#main > h1').click(function(){
+    show_section('#home');
+  });
+
+  // if #section present, show it
+  var url = document.location.toString(),
+      x = url.indexOf('#'),
+      sec = '#';
+  if (x > -1) sec = url.slice(x);
+  if (sec == '#') sec = '#home';
+  show_section(sec);
+
+  // remove initial .hidden from non-#home sections
+  $('#main > div').removeClass('hidden');
+
+
+});
